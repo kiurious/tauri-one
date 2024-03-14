99f76ba60cb259b6f1a98918a2d134478bb1a9e7
kiurious <kiurinegrao@gmail.com>
2024-03-14 12:20:46 +0200

    Add step prop to temperature range input

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -73,6 +73,7 @@ const Settings = () => {
             type="range"
             min="0"
             max="1"
+            step={0.01}
             value={temperature}
             className="range range-xs"
             onChange={(e) => {
